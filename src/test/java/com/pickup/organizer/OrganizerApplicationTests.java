cd461aa429929c59df70cb84b964905dcb07d1d9
Luca Ciccale <luca.ciccale@gmail.com>
2026-03-14 03:00:07 +0100

    Add Spring Boot initial project files

---
+++ src/test/java/com/pickup/organizer/OrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickup.organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
