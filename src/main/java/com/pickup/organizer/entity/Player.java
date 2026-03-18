81f27e09c20e31af298b9e105157218292b32b3f
Luca Ciccale <luca.ciccale@gmail.com>
2026-03-18 23:09:23 +0100

    Fix player entity

--- src/main/java/com/pickup/organizer/entity/Player.java
+++ src/main/java/com/pickup/organizer/entity/Player.java
@@ -2,64 +2,44 @@ package com.pickup.organizer.entity;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required!")
     private String name;
+
+    @NotBlank(message = "Last name is required!")
     private String lastName;
+
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Must be a valid email address")
     private String email;
+
+    @NotBlank(message = "Password is required!")
     private String password;
+
+    @NotNull(message = "Birth date is required!")
+    @Past(message = "Must be a valid birth date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
-
-    public Player() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-    public void setBirthDate(LocalDate birhDate) {
-        this.birthDate = birhDate;
-    }
 }
-
