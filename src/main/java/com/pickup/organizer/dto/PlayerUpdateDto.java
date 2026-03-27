92894ee4296f9a7d8a695c654527614af9d7af7f
Luca Ciccale <luca.ciccale@gmail.com>
2026-03-27 22:34:18 +0100

    Create PlayerUpdateDto for player updates

---
+++ src/main/java/com/pickup/organizer/dto/PlayerUpdateDto.java
@@ -0,0 +1,31 @@
+package com.pickup.organizer.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PlayerUpdateDto {
+    
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Name must only contain letters.")
+    private String name;
+
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Last name must only contain letters.")
+    private String lastName;
+
+    @Email(message = "Must be a valid email address.")
+    private String email;
+
+    @Past(message = "Must be a valid birth date.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+}
