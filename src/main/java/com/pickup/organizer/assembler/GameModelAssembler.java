12e3e8199d98d33076c7d95ec6d377b1763992ad
Luca Ciccale <201406929+lucaciccale@users.noreply.github.com>
2026-04-17 16:44:39 +0200

    Add cancellable game link to GameModelAssembler

--- src/main/java/com/pickup/organizer/assembler/GameModelAssembler.java
+++ src/main/java/com/pickup/organizer/assembler/GameModelAssembler.java
@@ -1,13 +1,18 @@
 package com.pickup.organizer.assembler;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import static com.pickup.organizer.service.GameService.MIN_TIME_IN_ADVANCE_HRS;
+
 import com.pickup.organizer.controller.GameController;
 import com.pickup.organizer.entity.Game;
+import com.pickup.organizer.enums.GameStatus;
 import com.pickup.organizer.model.GameModel;
 
 @Component
@@ -25,8 +30,22 @@ public class GameModelAssembler extends RepresentationModelAssemblerSupport<Game
             game.getMaxPlayers(),
             game.getStatus()
         );
-        model.add(linkTo(methodOn(GameController.class).getGame(game.getId())).withSelfRel());
+
+        model.add(linkTo(methodOn(GameController.class)
+            .getGame(game.getId())).withSelfRel());
+
+        if (isCancellable(game)) {
+            model.add(linkTo(methodOn(GameController.class)
+                .cancelGame(game.getId())).withRel("cancel"));
+        }
+
         return model;
+    }
+
+    private boolean isCancellable(Game game) {
+        LocalDateTime minAllowedTime = LocalDateTime.now().plusHours(MIN_TIME_IN_ADVANCE_HRS);
+        return game.getDateTime().isAfter(minAllowedTime)
+            && (game.getStatus() == GameStatus.OPEN || game.getStatus() == GameStatus.FULL);
     }
 
 }
